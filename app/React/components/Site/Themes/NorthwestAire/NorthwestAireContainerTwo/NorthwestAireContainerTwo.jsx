29083f8db9679d40ed54ad58b9f1ddde0b520761
Rocky Eastman Jr <eastmanrjr@gmail.com>
2016-10-19 14:42:46 -0700

    Production push

--- app/React/components/Site/Themes/NorthwestAire/NorthwestAireContainerTwo/NorthwestAireContainerTwo.jsx
+++ app/React/components/Site/Themes/NorthwestAire/NorthwestAireContainerTwo/NorthwestAireContainerTwo.jsx
@@ -74,7 +74,7 @@ class NorthwestAireContainerTwo extends React.Component {
     _introContainer() {
         return {
             style: {
-                margin: "5vh 0 5vh 0",
+                margin: "5vh 0 0 0",
                 width: "85vw",
                 display: "flex",
                 flexDirection: "column",
@@ -105,8 +105,10 @@ class NorthwestAireContainerTwo extends React.Component {
                 alignItems: "center",
                 fontWeight: "700",
                 fontSize: "6vw",
-                letterSpacing: "0.5vw",
+                letterSpacing: "0.6vw",
                 "@media (min-width: 48em) and (max-width: 64em)": {
+                    fontSize: "3.75vw",
+                    letterSpacing: "0.35vw"
                 },
                 "@media (min-width: 64em)": {
                     fontSize: "2vw",
@@ -125,7 +127,7 @@ class NorthwestAireContainerTwo extends React.Component {
     _introSmallText() {
         return {
             style: {
-                margin: "3vh 0 0 0",
+                margin: "5vh 0 0 0",
                 width: "100%",
                 display: "flex",
                 justifyContent: "flex-start",
@@ -134,6 +136,10 @@ class NorthwestAireContainerTwo extends React.Component {
                 fontSize: "4vw",
                 letterSpacing: "0.25vw",
                 "@media (min-width: 48em) and (max-width: 64em)": {
+                    margin: "2.5vh 0 0 0",
+                    width: "60%",
+                    fontSize: "2.5vw",
+                    letterSpacing: "0.15vw"
                 },
                 "@media (min-width: 64em)": {
                     width: "85%",
@@ -158,8 +164,94 @@ class NorthwestAireContainerTwo extends React.Component {
                 width: "35vw",
                 backgroundColor: "white",
                 "@media (min-width: 48em) and (max-width: 64em)": {
+                    margin: "2.5vh 0 0 0",
+                    width: "22vw",
                 },
                 "@media (min-width: 64em)": {
+                }
+            }
+        }
+    }
+
+    /**
+    * Settings for: _servicesContainer
+    *
+    * @function _servicesContainer
+    * @return {object}
+    */
+    _servicesContainer() {
+        return {
+            style: {
+                margin: "10vh 0 5vh 0",
+                width: "85vw",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "flex-statrt",
+                fontFamily: "Lato, sans-serif",
+                "@media (min-width: 48em) and (max-width: 64em)": {
+                },
+                "@media (min-width: 64em)": {
+                }
+            }
+        }
+    }
+
+    /**
+    * Settings for: _servicesLargeText
+    *
+    * @function _servicesLargeText
+    * @return {object}
+    */
+    _servicesLargeText() {
+        return {
+            style: {
+                width: "100%",
+                display: "flex",
+                justifyContent: "flex-start",
+                alignItems: "center",
+                fontWeight: "400",
+                fontSize: "4.5vw",
+                letterSpacing: "0.5vw",
+                "@media (min-width: 48em) and (max-width: 64em)": {
+                    fontSize: "2.5vw",
+                    letterSpacing: "0.35vw"
+                },
+                "@media (min-width: 64em)": {
+                    fontSize: "1.25vw",
+                    letterSpacing: "0.1875vw"
+                }
+            }
+        }
+    }
+
+    /**
+    * Settings for: _servicesSmallText
+    *
+    * @function _servicesSmallText
+    * @return {object}
+    */
+    _servicesSmallText() {
+        return {
+            style: {
+                margin: "2.5vh 0 0 0",
+                width: "100%",
+                display: "flex",
+                justifyContent: "flex-start",
+                alignItems: "center",
+                fontWeight: "300",
+                fontSize: "3.5vw",
+                letterSpacing: "0.25vw",
+                "@media (min-width: 48em) and (max-width: 64em)": {
+                    margin: "2.5vh 0 0 0",
+                    width: "60%",
+                    fontSize: "2.25vw",
+                    letterSpacing: "0.15vw"
+                },
+                "@media (min-width: 64em)": {
+                    width: "85%",
+                    fontSize: "1.15vw",
+                    letterSpacing: "0.125vw"
                 }
             }
         }
@@ -178,6 +270,9 @@ class NorthwestAireContainerTwo extends React.Component {
         let _introDivider = this._introDivider();
         let _introLargeText = this._introLargeText();
         let _introSmallText = this._introSmallText();
+        let _servicesContainer = this._servicesContainer();
+        let _servicesLargeText = this._servicesLargeText();
+        let _servicesSmallText = this._servicesSmallText();
         return (
             <div className="div" style={_div.style}>
                 <div className="introContainer" style={_introContainer.style}>
@@ -186,6 +281,12 @@ class NorthwestAireContainerTwo extends React.Component {
                     Northwest Aire Services, Inc. has been serving the Pacific Northwest since 2003.
                     </div>
                     <div className="introDivider" style={_introDivider.style}></div>
+                </div>
+                <div className="servicesContainer" style={_servicesContainer.style}>
+                    <div className="servicesLargeText" style={_servicesLargeText.style}>WHO WE ARE</div>
+                    <div className="servicesSmallText" style={_servicesSmallText.style}>
+                    We are a full service commercial HVAC contractor with the resources and motivation to provide industry-leading solutions. Equipped to competitively bid any job, it is our hope to build lifelong relationships with our customers - relationships that will lead to an enduring and mutual satisfaction.
+                    </div>
                 </div>
             </div>
         )
